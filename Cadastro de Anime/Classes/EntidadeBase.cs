dfb87fa476a15a127c677d2d0dd21db9ab3f40bd
GabrielDutraB <65261579+GabrielDutraB@users.noreply.github.com>
2021-09-16 20:14:34 -0300

    Add files via upload

---
+++ Cadastro de Anime/Classes/EntidadeBase.cs
@@ -0,0 +1,8 @@
+namespace Animes
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+
+    }
+}
---
+++ Cadastro de Anime/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+
+namespace Animes
+{
+    class Program
+    {
+        static AnimeRepositorio repositorio = new AnimeRepositorio();
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+			while (opcaoUsuario.ToUpper() != "X")
+			{
+				switch (opcaoUsuario)
+				{
+					case "1":
+						ListarAnimes();
+						break;
+					case "2":
+						InserirAnimes();
+						break;
+					case "3":
+						AtualizarAnimes();
+						break;
+					case "4":
+						ExcluirAnimes();
+						break;
+					case "5":
+						VisualizarAnimes();
+						break;
+					case "C":
+						Console.Clear();
+						break;
+
+					default:
+						throw new ArgumentOutOfRangeException();
+				}
+
+				opcaoUsuario = ObterOpcaoUsuario();
+			}
+
+			Console.WriteLine("Obrigado por utilizar nossos serviços.");
+			Console.ReadLine();
+        }
+
+        private static void ExcluirAnimes()
+		{
+			Console.Write("Digite o id do anime: ");
+			int indiceAnime = int.Parse(Console.ReadLine());
+
+			repositorio.Exclui(indiceAnime);
+		}
+
+        private static void VisualizarAnimes()
+		{
+			Console.Write("Digite o id do anime: ");
+			int indiceAnime = int.Parse(Console.ReadLine());
+
+			var anime = repositorio.RetornaPorId(indiceAnime);
+
+			Console.WriteLine(anime);
+		}
+
+        private static void AtualizarAnimes()
+		{
+			Console.Write("Digite o id do Anime: ");
+			int indiceAnime = int.Parse(Console.ReadLine());
+
+			// https://docs.microsoft.com/pt-br/dotnet/api/system.enum.getvalues?view=netcore-3.1
+			// https://docs.microsoft.com/pt-br/dotnet/api/system.enum.getname?view=netcore-3.1
+			foreach (int i in Enum.GetValues(typeof(Genero)))
+			{
+				Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
+			}
+			Console.Write("Digite o gênero entre as opções acima: ");
+			int entradaGenero = int.Parse(Console.ReadLine());
+
+			Console.Write("Digite o Título do Anime: ");
+			string entradaTitulo = Console.ReadLine();
+
+			Console.Write("Digite o Ano de Início do Anime: ");
+			int entradaAno = int.Parse(Console.ReadLine());
+
+			Console.Write("Digite a Descrição do Anime: ");
+			string entradaDescricao = Console.ReadLine();
+
+			Anime AtualizarAnimes = new Anime(id: indiceAnime,
+										genero: (Genero)entradaGenero,
+										titulo: entradaTitulo,
+										ano: entradaAno,
+										descricao: entradaDescricao);
+
+			repositorio.Atualiza(indiceAnime, AtualizarAnimes);
+		}
+        private static void ListarAnimes()
+		{
+			Console.WriteLine("Listar Animes");
+
+			var lista = repositorio.Lista();
+
+			if (lista.Count == 0)	
+			{
+				Console.WriteLine("Nenhum anime cadastrado.");
+				return; 
+			}
+
+			foreach (var anime in lista)
+	        { 
+				var excluido = anime.retornaExcluido();
+                
+				Console.WriteLine("#ID {0}: - {1} {2}", anime.retornaId(), anime.retornaTitulo(), (excluido ? "*Excluído*" : ""));
+			}
+		}
+
+        private static void InserirAnimes()
+		{
+			Console.WriteLine("Inserir novo anime");
+
+			// https://docs.microsoft.com/pt-br/dotnet/api/system.enum.getvalues?view=netcore-3.1
+			// https://docs.microsoft.com/pt-br/dotnet/api/system.enum.getname?view=netcore-3.1
+			foreach (int i in Enum.GetValues(typeof(Genero)))
+			{
+				Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
+			}
+			Console.Write("Digite o gênero entre as opções acima: ");
+			int entradaGenero = int.Parse(Console.ReadLine());
+
+			Console.Write("Digite o Título do Anime: ");
+			string entradaTitulo = Console.ReadLine();
+
+			Console.Write("Digite o Ano de Início do Anime: ");
+			int entradaAno = int.Parse(Console.ReadLine());
+
+			Console.Write("Digite a Descrição do Anime: ");
+			string entradaDescricao = Console.ReadLine();
+
+			Anime novoAluno = new Anime(id: repositorio.ProximoId(),
+										genero: (Genero)entradaGenero,
+										titulo: entradaTitulo,
+										ano: entradaAno,
+										descricao: entradaDescricao);
+
+			repositorio.Insere(novoAluno);
+		}
+
+        private static string ObterOpcaoUsuario()
+		{
+			Console.WriteLine();
+			Console.WriteLine("Animeflix a seu dispor!!!");
+			Console.WriteLine("Informe a opção desejada:");
+
+			Console.WriteLine("1- Listar animes");
+			Console.WriteLine("2- Inserir novo anime");
+			Console.WriteLine("3- Atualizar anime");
+			Console.WriteLine("4- Excluir anime");
+			Console.WriteLine("5- Visualizar anime");
+			Console.WriteLine("C- Limpar Tela");
+			Console.WriteLine("X- Sair");
+			Console.WriteLine();
+
+			string opcaoUsuario = Console.ReadLine().ToUpper();
+			Console.WriteLine();
+			return opcaoUsuario;
+		}
+    }
+}
